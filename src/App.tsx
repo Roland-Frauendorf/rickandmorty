b7b2d3247253bba414e00f5a10cd4688b3a7950f
Roland <roland.frauendorf.dev@gmail.com>
2022-01-15 14:17:01 -0300

    install and configure prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Em construção...
-      </p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>Em construção...</p>
+        </div>
+    );
 }
 
 export default App;
