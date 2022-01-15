516437b15dd5bab1bb05d5c3fc90b544af7415b1
Roland <roland.frauendorf.dev@gmail.com>
2022-01-15 17:01:53 -0300

    add apollo client and provider

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,8 +1,14 @@
 import React, { ReactNode } from 'react';
+import { ApolloProvider } from '@apollo/client';
 import ColorPropsProvider from './context/props/context.props.colors';
+import apolloClient from './GraphQL/client';
 
 const providers: React.FC = ({ children }: { children?: ReactNode }) => {
-    return <ColorPropsProvider>{children}</ColorPropsProvider>;
+    return (
+        <ApolloProvider client={apolloClient}>
+            <ColorPropsProvider>{children}</ColorPropsProvider>
+        </ApolloProvider>
+    );
 };
 
 export default providers;
