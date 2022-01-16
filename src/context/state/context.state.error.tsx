5264abbf9d6fc5ad5ea03f5068d1281b4a6c4502
Roland <roland.frauendorf.dev@gmail.com>
2022-01-15 22:37:05 -0300

    add error state context

---
+++ src/context/state/context.state.error.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, ReactNode, useContext, useState } from 'react';
+
+type ErrorType = {
+    loadError: boolean;
+    setLoadError: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+/**
+ * Estado de erros da página. Atualmente possui apenas o estado de
+ * erro de carregamento, mas outros tipos podem ser implementados.
+ * Sua usabilidade consiste em gravar que houve erro no carregamento
+ * do conteúdo de um componente para que os outros componentes do
+ * ecossistema não sejam renderizados e seja renderizada uma mensagem
+ * de erro.
+ */
+const ErrorContext = createContext<ErrorType>({
+    loadError: false,
+    setLoadError: () => {},
+});
+
+export default function ErrorProvider({ children }: { children: ReactNode }) {
+    const [loadError, setLoadError] = useState(false);
+
+    return (
+        <ErrorContext.Provider value={{ loadError, setLoadError }}>
+            {children}
+        </ErrorContext.Provider>
+    );
+}
+
+export const useErrorState = () => useContext(ErrorContext);
