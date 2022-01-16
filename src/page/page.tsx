421c9bd86b8c1f742ee15b6f1dc985f8fd977e7b
Roland <roland.frauendorf.dev@gmail.com>
2022-01-16 19:21:02 -0300

    add error on load content from GraphQL

--- src/page/page.tsx
+++ src/page/page.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
+import { Modal } from 'antd';
 import Style from './page.style';
-import { useColorProps } from '../context';
+import { useColorProps, useDimensionsProps, useErrorState } from '../context';
 import Paginator from './components/paginator/paginator';
+import { PageContainerProps } from './page.types';
+import Characters from './components/characters/characters';
 
-function Page() {
-    const { mainColor, fontMainColor, secondaryColor } = useColorProps();
-
-    return (
-        <Style.Window mainColor={mainColor} fontMainColor={fontMainColor}>
-            <Style.Container secondaryColor={secondaryColor}>
-                <Style.Header fontMainColor={fontMainColor}>
+const pageContainer: React.FC<PageContainerProps> = ({
+    mainColor,
+    contrastColor,
+    secondaryColor,
+    borderRadius,
+    loadError,
+}) => {
+    if (!loadError)
+        return (
+            <Style.Container secondaryColor={secondaryColor} borderRadius={borderRadius}>
+                <Style.Header contrastColor={contrastColor}>
                     Rick and Morty - Characters
                 </Style.Header>
-                <Style.Content>
+                <Style.Content mainColor={mainColor} contrastColor={contrastColor}>
+                    <Characters />
                     <Paginator />
                 </Style.Content>
             </Style.Container>
+        );
+
+    return (
+        <div>
+            {Modal.error({
+                title: 'Error!',
+                content: 'Something went wrong while trying to load the content. Try again later.',
+            })}
+        </div>
+    );
+};
+
+function Page() {
+    const { mainColor, contrastColor, secondaryColor } = useColorProps();
+    const { borderRadius } = useDimensionsProps();
+    const { loadError } = useErrorState();
+
+    return (
+        <Style.Window mainColor={mainColor} contrastColor={contrastColor}>
+            {pageContainer({ mainColor, contrastColor, secondaryColor, borderRadius, loadError })}
             <Style.Footer>by Roland Frauendorf</Style.Footer>
         </Style.Window>
     );
