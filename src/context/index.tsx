71f7f94a30b0da2ca49ac7b16e8ab2e25e1deabb
Roland <roland.frauendorf.dev@gmail.com>
2022-01-15 22:38:37 -0300

    add error state and pagination load providers

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,6 +1,8 @@
 import ColorPropsProvider, { useColorProps } from './props/context.props.colors';
 import PageNumberProvider, { usePageNumber } from './pagination/context.pagination.number';
+import PaginationLoadProvider, { usePaginationLoad } from './pagination/context.pagination.load';
+import ErrorProvider, { useErrorState } from './state/context.state.error';
 
-export { ColorPropsProvider, PageNumberProvider };
+export { ColorPropsProvider, PageNumberProvider, PaginationLoadProvider, ErrorProvider };
 
-export { useColorProps, usePageNumber };
+export { useColorProps, usePageNumber, usePaginationLoad, useErrorState };
--- src/providers.tsx
+++ src/providers.tsx
@@ -1,14 +1,23 @@
 import React, { ReactNode } from 'react';
 import { ApolloProvider } from '@apollo/client';
-import { ColorPropsProvider, PageNumberProvider } from './context';
+import {
+    ColorPropsProvider,
+    ErrorProvider,
+    PageNumberProvider,
+    PaginationLoadProvider,
+} from './context';
 import apolloClient from './GraphQL/client';
 
 const providers: React.FC = ({ children }: { children?: ReactNode }) => {
     return (
         <ApolloProvider client={apolloClient}>
-            <ColorPropsProvider>
-                <PageNumberProvider>{children}</PageNumberProvider>
-            </ColorPropsProvider>
+            <ErrorProvider>
+                <ColorPropsProvider>
+                    <PaginationLoadProvider>
+                        <PageNumberProvider>{children}</PageNumberProvider>
+                    </PaginationLoadProvider>
+                </ColorPropsProvider>
+            </ErrorProvider>
         </ApolloProvider>
     );
 };
